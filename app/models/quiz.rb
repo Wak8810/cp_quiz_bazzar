f9dec0489bee8fe14b298352f24e2185d7bb31a4
Wak8810 <231205192@ccmailg.meijo-u.ac.jp>
2025-03-19 15:16:45 +0900

    solved_quizモデル、コントローラの作成.contollers配下にquizzesが必要だったため、それらを作成

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,7 @@
 class Quiz < ApplicationRecord
     belongs_to :user
     has_many :quiz_options
+    has_meny :solved_quizzes
     validates :title, presence: true
     validates :content, presence: true
     validates :explanation, presence: true
