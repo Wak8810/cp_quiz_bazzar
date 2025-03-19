1550d2cc3df7d87fe8e8f2451c24bac19e2a8b34
Hitoshi-Noborikawa <dengchuanrenzhi@gmail.com>
2025-03-19 16:33:34 +0900

    fix routes typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :quizzes, %i[index, show] do
-    resources :solved_quizzes, %i[create] do
+  resources :quizzes, only: %i[index show] do
+    resources :solved_quizzes, only: %i[create] do
     end
   end
   resources :users do
