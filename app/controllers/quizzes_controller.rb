b48252d74b7f3f0d087f0ffedddda3984dda2d13
Wak8810 <231205192@ccmailg.meijo-u.ac.jp>
2025-03-21 03:19:37 +0900

    quizのソートを作成

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,10 +1,23 @@
 class QuizzesController < ApplicationController
     def index
-        @quizzes = Quiz.all
+        how_sort = sort_params[:sort]
+        if how_sort == "desc"
+            @quizzes = Quiz.order(created_at: :desc)
+        elsif how_sort == "like"
+            @quizzes = Quiz.like_desc
+        else
+            @quizzes = Quiz.all
+        end
     end
 
     def show
         @quiz = Quiz.find(params[:id])
         @solved_quiz = @quiz.solved_quizzes.build
+    end
+
+    private
+
+    def sort_params
+        params.permit(:sort)
     end
 end
